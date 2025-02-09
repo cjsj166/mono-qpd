7379242e59b0c5430eb7f3c6937db19b3f1cdb2a
cjsj166 <dohyung200@gmail.com>
2025-02-09 16:46:50 +0900

    evaluation misc fixed

--- train_mono_qpd.py
+++ train_mono_qpd.py
@@ -303,7 +303,7 @@ def train(args):
 
             total_steps += 1
 
-            if total_steps % batch_len == 0  or total_steps==1 or (args.stop_step is not None and total_steps >= args.stop_step):# and total_steps != 0:
+            if total_steps % (batch_len*5) == 0  or total_steps==1 or (args.stop_step is not None and total_steps >= args.stop_step):# and total_steps != 0:
                 epoch = int(total_steps/batch_len)
                 
                 model_save_path = os.path.join(args.save_path, timestamp, 'checkpoints', f'{epoch}_epoch_{total_steps}_{args.name}.pth')
@@ -318,9 +318,9 @@ def train(args):
                             'total_steps': total_steps,
                             # ... any other states you need
                             }, model_save_path)
-                
-                if total_steps % (batch_len*1) == 0:
-                    results = validate_QPD(model.module, iters=args.valid_iters, save_result=False, val_save_skip=30, input_image_num=args.input_image_num, image_set='validation', path=args.datasets_path, save_path=save_dir)
+
+                if total_steps % (batch_len*10) == 0:
+                    results = validate_QPD(model.module, iters=args.valid_iters, save_result=False, val_save_skip=30, input_image_num=args.input_image_num, image_set='validation', path='datasets/QP-Data', save_path=save_dir)
                         
                     if qpd_epebest>=results['epe']:
                         qpd_epebest = results['epe']
@@ -343,15 +343,12 @@ def train(args):
                     logging.info(f"Current Best Result qpd rmse epoch {qpd_rmseepoch}, result: {qpd_rmsebest}")
                     logging.info(f"Current Best Result qpd ai2 epoch {qpd_ai2epoch}, result: {qpd_ai2best}")
 
-                    results = validate_MDD(model.module, iters=args.valid_iters, save_result=False, val_save_skip=30, input_image_num=args.input_image_num, image_set='validation', path=args.datasets_path, save_path=save_dir)
+                    results = validate_MDD(model.module, iters=args.valid_iters, save_result=False, val_save_skip=30, input_image_num=args.input_image_num, image_set='test', path='datasets/MDD_dataset', save_path=save_dir)
 
-                    
                     if dpdisp_ai2best>=results['ai2']:
                         dpdisp_ai2best = results['ai2']
                         dpdisp_ai2epoch = epoch
                     
-                    logging.info(f"Current Best Result dpdisp epe epoch {dpdisp_epeepoch}, result: {dpdisp_epebest}")
-                    logging.info(f"Current Best Result dpdisp rmse epoch {dpdisp_rmseepoch}, result: {dpdisp_rmsebest}")
                     logging.info(f"Current Best Result dpdisp ai2 epoch {dpdisp_ai2epoch}, result: {dpdisp_ai2best}")
                     
                     named_results = {}
