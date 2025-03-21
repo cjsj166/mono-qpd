25208d250f423bf84eb390b68775604d5154a73e
cjsj166 <dohyung200@gmail.com>
2025-03-21 09:58:54 +0900

    extract_epoch exception logic added

--- exp_args_settings/utils/utils.py
+++ exp_args_settings/utils/utils.py
@@ -3,13 +3,20 @@ from pathlib import Path
 
 def extract_epoch(ckpt):
     ckpt = Path(ckpt)
-    return int(str(ckpt.name).split("_")[0])
+    try :
+        ckpt_num = int(str(ckpt.name).split("_")[0])
+        return ckpt_num
+    except ValueError as e:
+        print(f'{e}')
+        return None
 
 def get_latest_ckpt(path):
     pth = Path(path)
     ckpts = pth.glob("**/*.pth")
 
-    sorted_ckpts = sorted(ckpts, key=extract_epoch)
+    valid_ckpts = [ckpt for ckpt in ckpts if extract_epoch(ckpt)]
+    
+    sorted_ckpts = sorted(valid_ckpts, key=extract_epoch)
 
     if len(sorted_ckpts) == 0:
         return None
@@ -21,5 +28,8 @@ def get_latest_ckpt(path):
 def get_ckpts_in_dir(dir_path):
     dir_path = Path(dir_path)
     ckpts = dir_path.rglob('**/*.pth')
-    ckpts = sorted(ckpts, key=extract_epoch)
+
+    valid_ckpts = [ckpt for ckpt in ckpts if extract_epoch(ckpt)]
+
+    ckpts = sorted(valid_ckpts, key=extract_epoch)
     return ckpts 
