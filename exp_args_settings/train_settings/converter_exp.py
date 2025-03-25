1dcaae98b7ca98134d15549e12c01407767dc676
cjsj166 <dohyung200@gmail.com>
2025-03-25 11:01:34 +0900

    InterpQPDSetting50K added

--- exp_args_settings/train_settings/converter_exp.py
+++ exp_args_settings/train_settings/converter_exp.py
@@ -74,6 +74,24 @@ class InterpQPDSetting(TrainConfig):
     val_datasets: Tuple[str] = ('DPD-Disp',)
 
 @dataclass
+class InterpQPDSetting50K(TrainConfig):
+    num_steps: int = 50_000
+    batch_size: int = 16
+    image_size: Tuple[int, int] = (448, 448)
+    lr: int = 0.0002 * np.sqrt(16)
+    qpd_valid_bs: int = 4
+    qpd_test_bs: int = 4
+    real_qpd_bs: int = 4
+    dp_disp_bs: int = 4
+    val_save_skip: int = 1
+
+    restore_ckpt_da_v2: str = 'mono_qpd/Depth_Anything_V2/checkpoints/depth_anything_v2_vitl.pth'
+    save_path: str = 'result/train/InterpQPDSetting'
+    feature_converter: str = 'interp'
+    val_datasets: Tuple[str] = ('DPD-Disp',)
+
+
+@dataclass
 class Interp150K(TrainConfig):
     num_steps: int = 150000
     batch_size: int = 16
