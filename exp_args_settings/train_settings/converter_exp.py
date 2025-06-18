fae91a4a61580e4734c3d39f8f70bde25bbbbede
cjsj166 <dohyung200@gmail.com>
2025-06-18 16:13:09 +0900

    GRU2 setting added

--- exp_args_settings/train_settings/converter_exp.py
+++ exp_args_settings/train_settings/converter_exp.py
@@ -92,6 +92,46 @@ class InterpQPDSetting(TrainConfig):
     val_datasets: Tuple[str] = ('DPD-Disp',)
 
 @dataclass
+class GRU2(TrainConfig):
+    num_steps: int = 200000
+    batch_size: int = 4
+    image_size: Tuple[int, int] = (448, 448)
+    lr: int = 0.0002
+    qpd_valid_bs: int = 4
+    qpd_test_bs: int = 4
+    real_qpd_bs: int = 4
+    dp_disp_bs: int = 1
+    val_save_skip: int = 1
+    train_iters: int = 2
+    valid_iters: int = 2
+
+    restore_ckpt_da_v2: str = 'mono_qpd/Depth_Anything_V2/checkpoints/depth_anything_v2_vitl.pth'
+    save_path: str = 'result/train/InterpQPDSetting'
+    feature_converter: str = 'interp'
+    val_datasets: Tuple[str] = ('DPD-Disp',)
+
+@dataclass
+class LocalGRU2(TrainConfig):
+    num_steps: int = 200000
+    batch_size: int = 1
+    image_size: Tuple[int, int] = (448, 448)
+    lr: int = 0.0002
+    qpd_valid_bs: int = 1
+    qpd_test_bs: int = 1
+    real_qpd_bs: int = 1
+    dp_disp_bs: int = 1
+    val_save_skip: int = 1
+    train_iters: int = 2
+    valid_iters: int = 2
+
+    restore_ckpt_da_v2: str = 'mono_qpd/Depth_Anything_V2/checkpoints/depth_anything_v2_vitl.pth'
+    save_path: str = 'result/train/InterpQPDSetting'
+    feature_converter: str = 'interp'
+    val_datasets: Tuple[str] = ('DPD-Disp',)
+
+
+
+@dataclass
 class InterpQPDSetting50K(TrainConfig):
     num_steps: int = 50_000
     batch_size: int = 16
