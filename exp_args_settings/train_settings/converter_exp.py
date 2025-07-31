2670f36e9809c0cf15ec9de5ac12fc8ae80e1837
cjsj166 <dohyung200@gmail.com>
2025-07-31 13:21:09 +0900

    Deleted cl cr volume

--- exp_args_settings/train_settings/converter_exp.py
+++ exp_args_settings/train_settings/converter_exp.py
@@ -261,7 +261,7 @@ class LocalVolumeLR(TrainConfig):
     val_datasets: Tuple[str] = ('DPD-Disp',)
 
 @dataclass
-class DispVolumeLR(TrainConfig):
+class OnlyDispVolumeLR(TrainConfig):
     num_steps: int = 200000
     batch_size: int = 4
     image_size: Tuple[int, int] = (448, 448)
@@ -277,7 +277,7 @@ class DispVolumeLR(TrainConfig):
     val_datasets: Tuple[str] = ('DPD-Disp',)
 
 @dataclass
-class LocalDispVolumeLR(TrainConfig):
+class LocalOnlyDispVolumeLR(TrainConfig):
     num_steps: int = 200000
     batch_size: int = 1
     image_size: Tuple[int, int] = (448, 448)
