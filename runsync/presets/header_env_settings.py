24f38cf8df4a0349ea6cff96b8ec7b590143c4fe
cjsj166 <dohyung200@gmail.com>
2025-08-26 21:05:57 +0900

    train script test time changed

--- runsync/presets/header_env_settings.py
+++ runsync/presets/header_env_settings.py
@@ -15,7 +15,7 @@ class BaseConfig:
 class FMDPTrain(BaseConfig):
     """Header env setting for FMDP jobs"""
     node_type: str = "gpu_1"   # 요청하신 job script와 동일
-    running_time: str = "00:05:00"
+    running_time: str = "00:10:00"
     env_name: str = "mono-qpd"  # conda activate mono-qpd
     cuda_version: str = "cuda/12.1"
     cudnn_version: str = "cudnn/9.0.0"
--- runsync/runsync_script.py
+++ runsync/runsync_script.py
@@ -160,7 +160,7 @@ cd {exec_path}
 
     # 재제출 시간 (운영: 23h55m). 지금은 테스트 값.
     # limit_sec = 23 * 3600 + 55 * 60
-    limit_sec = 4 * 60
+    limit_sec = 9 * 60
 
     # ---- pack 스크립트(학습 + watcher + restarter) 본문 ----
     pack_script_text = f"""#!/bin/bash
