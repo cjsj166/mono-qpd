b8dbc74fd5b63b6ac36fdc928d5e7fa834d0e9ce
cjsj166 <dohyung200@gmail.com>
2025-09-05 18:49:07 +0900

    made it executable with more than one batch

--- mono_qpd/mono_qpd.py
+++ mono_qpd/mono_qpd.py
@@ -73,9 +73,11 @@ class MonoQPD(nn.Module):
         assert h % 224 == 0 and w % 224 == 0, "Image dimensions must be multiples of 224"
         # image1_resized = self.resize_to_14_multiples(image1)
 
-        # center_normalized = self.normalize_image(image1)
-        left_normalized = self.normalize_image(image2[0:1])
-        right_normalized = self.normalize_image(image2[1:])
+        center_normalized = self.normalize_image(image1)
+        b = image1.shape[0]
+        left_normalized = self.normalize_image(image2[0:b])
+        right_normalized = self.normalize_image(image2[b:])
+
         # enc_features, depth = self.da_v2(image1_normalized) # Original
         if self.da_v2_output_condition == 'enc_features':
             # c_features = self.da_v2(center_normalized)
