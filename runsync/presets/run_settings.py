b6155357c95b822c64b95dc8555fac254bcab66a
cjsj166 <dohyung200@gmail.com>
2025-09-07 22:55:12 +0900

    Exp0907CLCRLRWoFFA

--- runsync/presets/run_settings.py
+++ runsync/presets/run_settings.py
@@ -85,7 +85,7 @@ class BaseConfig:
 
 
 @dataclass
-class Exp0907CLCRWoFFA(BaseConfig):
+class Exp0907CLCRLRWoFFA(BaseConfig):
     num_steps: int = 200000
     batch_size: int = 4
     image_size: Tuple[int, int] = (448, 448)
@@ -101,7 +101,7 @@ class Exp0907CLCRWoFFA(BaseConfig):
     val_datasets: Tuple[str] = ('DPD-Disp',)
 
 @dataclass
-class LocalExp0907CLCRWoFFA(BaseConfig):
+class LocalExp0907CLCRLRWoFFA(BaseConfig):
     num_steps: int = 200_000 # 200_000
     batch_size: int = 1
     image_size: Tuple[int, int] = (448, 448)
