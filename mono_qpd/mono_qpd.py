1f3dc11086bd640b2d2f7e407d1322de9907f278
cjsj166 <dohyung200@gmail.com>
2025-09-09 15:05:25 +0900

    Exp0909CLRContextLRCorr

--- mono_qpd/mono_qpd.py
+++ mono_qpd/mono_qpd.py
@@ -80,23 +80,23 @@ class MonoQPD(nn.Module):
 
         # enc_features, depth = self.da_v2(image1_normalized) # Original
         if self.da_v2_output_condition == 'enc_features':
-            # c_features = self.da_v2(center_normalized)
-            # c_features = c_features[1:]
+            c_features = self.da_v2(center_normalized)
+            c_features = c_features[1:]
             l_features = self.da_v2(left_normalized)
             l_features = l_features[1:]
             r_features = self.da_v2(right_normalized)
             r_features = r_features[1:]
-        
-        lr_features = [torch.cat([l, r], dim=1) for l, r in zip(l_features, r_features)]
-        lr_features = self.feature_converter(lr_features)
+
+        clr_features = [torch.cat([c, l, r], dim=1) for c, l, r in zip(c_features, l_features, r_features)]
+        clr_features = self.feature_converter(clr_features)
         # for f in ret_features:
         #     print(f.shape)
-        lr_features = lr_features[::-1] # Reverse the order of the features
+        clr_features = clr_features[::-1] # Reverse the order of the features
 
         if test_mode:
-            original_disp, upsampled = self.qpdnet(lr_features, image1, image2, iters=iters, test_mode=test_mode, flow_init=None)
+            original_disp, upsampled = self.qpdnet(clr_features, image1, image2, iters=iters, test_mode=test_mode, flow_init=None)
             return original_disp, upsampled
         else:
-            disp_predictions = self.qpdnet(lr_features, image1, image2, iters=iters, test_mode=test_mode, flow_init=None)
+            disp_predictions = self.qpdnet(clr_features, image1, image2, iters=iters, test_mode=test_mode, flow_init=None)
             return disp_predictions
 
--- runsync/presets/run_settings.py
+++ runsync/presets/run_settings.py
@@ -85,7 +85,7 @@ class BaseConfig:
 
 
 @dataclass
-class Exp0830LRContextLRCorr(BaseConfig):
+class Exp0909CLRContextLRCorr(BaseConfig):
     num_steps: int = 200000
     batch_size: int = 4
     image_size: Tuple[int, int] = (448, 448)
@@ -101,7 +101,7 @@ class Exp0830LRContextLRCorr(BaseConfig):
     val_datasets: Tuple[str] = ('DPD-Disp',)
 
 @dataclass
-class LocalExp0830LRContextLRCorr(BaseConfig):
+class LocalExp0909CLRContextLRCorr(BaseConfig):
     num_steps: int = 200_000 # 200_000
     batch_size: int = 1
     image_size: Tuple[int, int] = (448, 448)
