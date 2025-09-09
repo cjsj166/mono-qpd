8767c4614158f5225dfb47db9177a64e89d0ecdc
cjsj166 <dohyung200@gmail.com>
2025-09-09 11:43:01 +0900

    Exp0909BiLR

--- runsync/presets/run_settings.py
+++ runsync/presets/run_settings.py
@@ -85,7 +85,7 @@ class BaseConfig:
 
 
 @dataclass
-class Exp0829CLCRBiLR(BaseConfig):
+class Exp0909BiLR(BaseConfig):
     num_steps: int = 200000
     batch_size: int = 4
     image_size: Tuple[int, int] = (448, 448)
@@ -101,7 +101,7 @@ class Exp0829CLCRBiLR(BaseConfig):
     val_datasets: Tuple[str] = ('DPD-Disp',)
 
 @dataclass
-class LocalExp0829CLCRBiLR(BaseConfig):
+class LocalExp0909BiLR(BaseConfig):
     num_steps: int = 200_000 # 200_000
     batch_size: int = 1
     image_size: Tuple[int, int] = (448, 448)
