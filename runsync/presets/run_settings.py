561958cc70681da09d659c6ac716dd7216d48480
cjsj166 <dohyung200@gmail.com>
2025-09-20 11:43:33 +0900

    N116, N132 added

--- runsync/presets/run_settings.py
+++ runsync/presets/run_settings.py
@@ -84,6 +84,42 @@ class BaseConfig:
         self.save_path = str(save_path)
 
 @dataclass
+class Exp0916AdjustCLCRLRN132(BaseConfig):
+    num_steps: int = 200000
+    batch_size: int = 4
+    image_size: Tuple[int, int] = (448, 448)
+    lr: int = 0.0002
+    qpd_valid_bs: int = 4
+    qpd_test_bs: int = 4
+    real_qpd_bs: int = 4
+    dp_disp_bs: int = 2
+    val_save_skip: int = 1
+    clcr_weight: float = 0.03125
+
+    restore_ckpt_da_v2: str = 'mono_qpd/Depth_Anything_V2/checkpoints/depth_anything_v2_vitl.pth'
+    feature_converter: str = 'interp'
+    val_datasets: Tuple[str] = ('DPD-Disp',)
+
+
+@dataclass
+class Exp0916AdjustCLCRLRN116(BaseConfig):
+    num_steps: int = 200000
+    batch_size: int = 4
+    image_size: Tuple[int, int] = (448, 448)
+    lr: int = 0.0002
+    qpd_valid_bs: int = 4
+    qpd_test_bs: int = 4
+    real_qpd_bs: int = 4
+    dp_disp_bs: int = 2
+    val_save_skip: int = 1
+    clcr_weight: float = 0.0625
+
+    restore_ckpt_da_v2: str = 'mono_qpd/Depth_Anything_V2/checkpoints/depth_anything_v2_vitl.pth'
+    feature_converter: str = 'interp'
+    val_datasets: Tuple[str] = ('DPD-Disp',)
+
+
+@dataclass
 class Exp0916AdjustCLCRLRN18(BaseConfig):
     num_steps: int = 200000
     batch_size: int = 4
