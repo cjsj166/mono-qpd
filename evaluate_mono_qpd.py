705c989763811531e57627d18316a7e899792812
cjsj166 <dohyung200@gmail.com>
2026-01-13 15:58:33 +0900

    Added QPDv2-Test

--- evaluate_mono_qpd.py
+++ evaluate_mono_qpd.py
@@ -1274,7 +1274,7 @@ if __name__ == '__main__':
     parser.add_argument('--exp_name', default='Interp', help="name your experiment")
     parser.add_argument('--ckpt_epoch', type=str, default=0)
     parser.add_argument('--save_result', action='store_true', help="Save predicted results")
-    parser.add_argument('--eval_datasets', choices=['QPDv2-Valid', 'QPD-Test', 'QPD-TransDisk-Test', 'QPD-FStop-1_2-Test', 'QPD-FStop-1_4-Test', 'QPD-FStop-2_0-Test', 'QPD-FStop-2_8-Test', 'QPD-FStops-All', 'QPD-Valid', 'DPD_Disp', 'Real_QPD', 'QPD-Test-noise', 'DP5K-Valid', 'DP5K-Test', 'DP5K-Test-Lowres', 'DP119', 'Make_QPD', 'Make_DDDP'], nargs='+', default=[], required=True, help="Additional dataset to evaluate")
+    parser.add_argument('--eval_datasets', choices=['QPDv2-Test', 'QPDv2-Valid', 'QPD-Test', 'QPD-TransDisk-Test', 'QPD-FStop-1_2-Test', 'QPD-FStop-1_4-Test', 'QPD-FStop-2_0-Test', 'QPD-FStop-2_8-Test', 'QPD-FStops-All', 'QPD-Valid', 'DPD_Disp', 'Real_QPD', 'QPD-Test-noise', 'DP5K-Valid', 'DP5K-Test', 'DP5K-Test-Lowres', 'DP119', 'Make_QPD', 'Make_DDDP'], nargs='+', default=[], required=True, help="Additional dataset to evaluate")
 
     args = parser.parse_args()
 
@@ -1545,6 +1545,25 @@ if __name__ == '__main__':
             named_result[f'val_qpd_v2_valid/{k}'] = v
             if 'img' not in k:
                 print(f'val_qpd_v2_valid/{k}: {v}')
+
+        logger.write_dict(named_result)
+        logger.close()
+
+    if 'QPDv2-Test' in args.eval_datasets:
+        save_dir = os.path.join(conf.save_path, 'qpdv2-test')
+        save_path = os.path.join(save_dir, f'{epoch:03d}_epoch')
+        print(save_path)
+
+        result = validate_QPD(model, iters=conf.valid_iters, mixed_prec=use_mixed_precision, save_result=True if args.save_result else False, datatype = conf.datatype, image_set="test", path='datasets/QP-Data-v2', save_path=save_path, batch_size=conf.qpd_valid_bs if conf.qpd_valid_bs else 1)
+        
+        log_dir = os.path.join(save_dir, 'runs') 
+        logger = EvalLogger(log_dir=log_dir, epoch=epoch) # epoch=checkpoint['total_steps'])
+        
+        named_result = {}
+        for k, v in result.items():
+            named_result[f'val_qpd_v2_test/{k}'] = v
+            if 'img' not in k:
+                print(f'val_qpd_v2_test/{k}: {v}')
 
         logger.write_dict(named_result)
         logger.close()
